d0aea1f8ed6a8d02c461ea0c6fe5412d00cc4e10
sivarajesh17 <sivarajeshsigireddy7032@gmail.com>
2025-09-03 22:27:52 +0530

    Basics of python

---
+++ Day1/Introduction.py
@@ -0,0 +1,36 @@
+#Basic Arithemetic Operations
+a=3
+b=4
+print("Addition=",a+b)
+print("Subtraction=",a-b)
+print("Multiplication=",a*b)
+print("Modulus=",a%b)
+print("Division=",a/b)
+print("Exponential=",a**b)
+print("Floor division=",a//b)
+#Strings
+Name='Siva Rajesh'
+print('Name:',Name)
+print('I am enjoying 30 days of python')
+#DataTypes
+print("10 =>",type(10))
+print("9.8 =>",type(9.8))
+print("3.14 =>",type(3.14))
+print("4-4j =>",type(4-4j))
+print("Sivarajesh =>",type('Sivarajesh'))
+print('True =>',type(True))
+# List
+list_data = [1, 2, 3, "Python", 9.8]
+print(list_data, "->", type(list_data))
+
+# Tuple
+tuple_data = (1, 2, 3, "AI", False)
+print(tuple_data, "->", type(tuple_data))
+
+# Set
+set_data = {1, 2, 3, 3, 4, 5}
+print(set_data, "->", type(set_data))
+
+# Dictionary
+dict_data = {"name": "Sivarajesh", "country": "India", "language": "Python"}
+print(dict_data, "->", type(dict_data))
