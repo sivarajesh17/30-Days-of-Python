cf9bb51253e98ffa947cc3fbf4763c15c83fb8df
sivarajesh17 <sivarajeshsigireddy7032@gmail.com>
2025-09-04 22:45:48 +0530

    Built in Functions

---
+++ Day2/Variables.py
@@ -0,0 +1,38 @@
+#Day 2: 30 Days of Python Programmming
+#Exercise:LEVEL 1
+first_name='Sivarajesh'
+last_name='Sigireddy'
+full_name='Sivarajesh Sigireddy'
+country='India'
+city='Tuni'
+age=22
+year=2003
+is_married=False
+is_true=True
+is_ligh_on=True
+a,b,c,d=10,20,25,36
+#Exercise:LEVEL 2
+print('first_name',type(first_name))
+print('age',type(age))
+print('is_married',type(is_married))
+print(len(first_name))
+num_one,num_two=5,4
+total=num_one+num_two
+diff=num_one-num_two
+product=num_one*num_two
+divison=num_one/num_two
+remainder=num_one%num_two
+exp=num_one**num_two
+floor_division=num_one//num_two
+radius=30
+area_of_circle=((3.14)*(radius**2))
+circum_of_circle=(2*3.14*radius)
+print(total)
+print(diff)
+print(product)
+print(divison)
+print(remainder)
+print(exp)
+print(floor_division)
+print(area_of_circle)
+print(circum_of_circle)
